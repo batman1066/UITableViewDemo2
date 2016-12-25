b188a2a6cec39a7adb21c7b1ea218365ac421067
yang ming <yangming@bogon>
2016-12-25 14:16:00 +0800

    Initial Commit

---
+++ UITableViewDemo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewDemo2
+//
+//  Created by yang ming on 2016/12/25.
+//  Copyright © 2016年 yang ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
